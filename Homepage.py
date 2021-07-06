0d0f1258de6a856b58f2fbbdbf40ca8fab7b6f59
ArvindVinodh <arvindvinodh2003@gmail.com>
2021-07-06 21:34:39 +0300

    Full code for Homepage

--- Homepage.py
+++ Homepage.py
@@ -1,2 +1,30 @@
-print("*********************Reminders*********************")
-print("Your Reminders:")
+from Add import *
+from Retrieve import *
+from Delete import *
+import os.path
+if os.path.isfile("Reminder.txt") != True:
+    fout=open("Reminder.txt","a")
+    fout.close()
+def homepage():
+    while True:
+        print("*********************Reminders*********************")
+        print()
+        print("             ********Homepage********              ")
+        print()
+        print("Your Reminders:")
+        retrieve()
+        print()
+        print("             ********Menu********              ")
+        print("1.Add Reminder")
+        print("2.Delete Reminder")
+        print("3.Exit")
+        ques=int(input("Enter your choice: "))
+        if ques == 1:
+            add()
+        elif ques == 2:
+            delete()
+        else:
+            return
+homepage()
+
+
