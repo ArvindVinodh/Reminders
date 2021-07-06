c040498eb0137196bea0be0212a93758b0f8a8eb
ArvindVinodh <arvindvinodh2003@gmail.com>
2021-07-06 20:04:48 +0300

    Modified Add Function(While True)

--- Add.py
+++ Add.py
@@ -1,9 +1,17 @@
-def add(N):
+def add():
+    print("             ********New Reminder********              ")
     fout=open("Reminder.txt","a")
-    for i in range(N):
-        n=str(i+1)
+    i=1
+    while True:
+        n=str(i)
         rem=input("Enter Reminder "+n+": ")
         fout.write(n+"). "+rem+"\n")
-    print("Reminder entered")
-    ques=input("Press Any Key to Continue")
+        ques1=input("Do you want to enter another reminder? Y/N")
+        if ques1 == "Yy":
+            i+=1
+            continue
+        else:
+            break
+    print("Reminder(s) entered")
+    ques2=input("Press Any Key to go back to Homepage")
     return
