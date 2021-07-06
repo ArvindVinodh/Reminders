ec38c9dacbd4c14102025485285feb24c5bf131d
ArvindVinodh <arvindvinodh2003@gmail.com>
2021-07-06 20:39:44 +0300

    Modified Retrieve Function(\n removed)

--- Retrieve.py
+++ Retrieve.py
@@ -1,5 +1,10 @@
 def retrieve():
     fin=open("Reminder.txt","r")
-    Str=fin.read()
-    print(Str)
+    Str=fin.readlines()
+    for j in Str:
+        new_Str=""
+        for k in j:
+            if k not in "\n":
+                new_Str+=k
+        print(new_Str)
     return
