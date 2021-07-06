78e8df4da8eb2f6ddfa310f6e04131f1deb8e197
ArvindVinodh <arvindvinodh2003@gmail.com>
2021-07-06 20:42:44 +0300

    Modified Add Function(maybe in 'Yy')

--- Add.py
+++ Add.py
@@ -1,17 +1,28 @@
 def add():
     print("             ********New Reminder********              ")
     fout=open("Reminder.txt","a")
-    i=1
+    I=1
+    N=Line()
     while True:
-        n=str(i)
-        rem=input("Enter Reminder "+n+": ")
+        n=str(N)
+        i=str(I)
+        rem=input("Enter Reminder "+i+": ")
         fout.write(n+"). "+rem+"\n")
         ques1=input("Do you want to enter another reminder? Y/N")
-        if ques1 == "Yy":
-            i+=1
+        if ques1 in "Yy":
+            I+=1
+            N+=1
             continue
         else:
             break
+    fout.close()
     print("Reminder(s) entered")
     ques2=input("Press Any Key to go back to Homepage")
     return
+
+def Line():
+    fin=open("Reminder.txt","r")
+    count=len(fin.readlines())
+    fin.close()
+    return count+1
+        
